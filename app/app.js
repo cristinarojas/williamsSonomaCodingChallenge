6905f47684189ec5472d8b057818fb7c0b30413c
Cristina Rojas <telematicacristina@gmail.com>
2019-03-06 15:39:38 -0800

    Adding eventlistener

--- app/app.js
+++ app/app.js
@@ -1,7 +1,26 @@
-/* Consumes the JSON of products code */
-console.log('original-->', data);
+/*IFEE */
+(function() {
+  /* Consumes the JSON of products code */
+  console.log('original-->', data);
 
 
-// ES5 Destructuring
-const { groups } = data;
-console.log('groups-->', groups);
+  // ES5 Destructuring
+  const { groups } = data;
+  console.log('groups-->', groups);
+
+  const productOverlayElement = document.getElementsByClassName('overlayProduct');
+  const carouselProductsElement = document.getElementById('carousel-products');
+
+  var showCarousel = function() {
+      var parentNodeElement = this.parentNode.id;
+      console.log(parentNodeElement);
+      carouselProductsElement.classList.add("showCarousel");
+  };
+
+  // Adding the event listener to the overlayProduct element
+  // then calling the function showCarousel to obtain the clicked parent clicked id
+  for (let i = 0; i < productOverlayElement.length; i++) {
+      productOverlayElement[i].addEventListener('click', showCarousel, false);
+  }
+
+})();
