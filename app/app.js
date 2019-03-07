65e5d79b274be5b1ac4bfa1b6813bbfa0dda13fe
Cristina Rojas <telematicacristina@gmail.com>
2019-03-06 17:14:50 -0800

    Adding bootstrap just for carousel section

--- app/app.js
+++ app/app.js
@@ -2,19 +2,20 @@
 (function() {
   /* Consumes the JSON of products code */
   console.log('original-->', data);
-
-
   // ES5 Destructuring
   const { groups } = data;
   console.log('groups-->', groups);
 
+
+  // Obtaining all the DOM elements
   const productOverlayElement = document.getElementsByClassName('overlayProduct');
   const carouselProductsElement = document.getElementById('carousel-products');
+  const closeElement = document.getElementById('close');
 
-  var showCarousel = function() {
-      var parentNodeElement = this.parentNode.id;
-      console.log(parentNodeElement);
-      carouselProductsElement.classList.add("showCarousel");
+  // Function sho show carouse
+  let showCarousel = function() {
+      let parentNodeElement = this.parentNode.id; // Obtaining the clicked id product
+      carouselProductsElement.classList.add("showCarousel"); // Adding class to show carousel
   };
 
   // Adding the event listener to the overlayProduct element
@@ -22,5 +23,11 @@
   for (let i = 0; i < productOverlayElement.length; i++) {
       productOverlayElement[i].addEventListener('click', showCarousel, false);
   }
+
+  // Handle close
+  closeElement.addEventListener('click', function() {
+    console.log('here');
+    carouselProductsElement.classList.add("hideCarousel");
+  }, false);
 
 })();
